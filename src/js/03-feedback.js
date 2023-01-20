2eaea7db1d3f968d1c3b44501f59cdb977490be1
smolianko2503 <smolianko.andrey1993@gmail.com>
2023-01-20 19:30:31 +0200

    update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,34 +2,44 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const KEY_LOCAL = "feedback-form-state";
-const dataInput = {};
-
-
-function onInput(evt) {
-    dataInput[evt.target.name] = evt.target.value;
-    const stringifyDataInput = JSON.stringify(dataInput);
-  localStorage.setItem(KEY_LOCAL, stringifyDataInput);
-}
-
-function onClickBtn(evt) {
-  const parsedDataInput = JSON.parse(localStorage.getItem(KEY_LOCAL));
-  console.log(parsedDataInput);
-
-  evt.preventDefault();
-  localStorage.removeItem(KEY_LOCAL);
-  evt.currentTarget.reset();
-
-  delete dataInput.email;
-  delete dataInput.message;
-}
 
 form.addEventListener('input', throttle(onInput, 500));
 form.addEventListener('submit', onClickBtn);
 
-if (localStorage.getItem(KEY_LOCAL)) {
-    const parsedDataInput = JSON.parse(localStorage.getItem(KEY_LOCAL));
-    form.elements.email.value = parsedDataInput.email || '';
-    form.elements.message.value = parsedDataInput.message;
+const dataInput = JSON.parse(localStorage.getItem(KEY_LOCAL)) || {};;
+
+function onInput(evt) {
+  
+  dataInput[evt.target.name] = evt.target.value;
+  const stringifyDataInput = JSON.stringify(dataInput);
+  localStorage.setItem(KEY_LOCAL, stringifyDataInput);
 };
 
+function onClickBtn(evt) {
 
+  evt.preventDefault();
+
+  if (
+    form.elements.email.value === '' ||
+    form.elements.message.value === ''
+  ) {
+    alert('Fill in all fields');
+  } else {
+    console.log(dataInput);
+    localStorage.removeItem(KEY_LOCAL);
+    evt.currentTarget.reset();
+    
+    delete dataInput.email;
+    delete dataInput.message;
+  }
+  
+};
+
+if (localStorage.getItem(KEY_LOCAL)) {
+  const parsedDataInput = JSON.parse(localStorage.getItem(KEY_LOCAL));
+  if (parsedDataInput) {
+    form.elements.email.value = parsedDataInput.email || '';
+    form.elements.message.value = parsedDataInput.message || '';
+  }
+};
+ 
